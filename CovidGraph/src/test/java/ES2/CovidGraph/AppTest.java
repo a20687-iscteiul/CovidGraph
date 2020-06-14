7191b34ab6f871d61e1f0ce974e65283fc853cb7
PedroSantiago <a20687@iscte-iul.pt>
2020-06-14 21:16:22 +0100

    Creating tests

--- CovidGraph/src/test/java/ES2/CovidGraph/AppTest.java
+++ CovidGraph/src/test/java/ES2/CovidGraph/AppTest.java
@@ -1,38 +1,63 @@
 package ES2.CovidGraph;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.io.File;
+import java.io.IOException;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.eclipse.jgit.api.Git;
+import org.junit.Before;
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@Before
+	public void setUp() throws Exception {
+		File f = new File("git/");
+		Git g = Git.open(f);
+		
+	}
+
+	@Test
+	public void testCloneGit() throws IOException {
+		File f = new File("git/");
+		Git g = Git.open(f);
+		assertEquals("class org.eclipse.jgit.api.Git",g.getClass().toString());
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRDFfile() throws IOException {
+		Git g = Git.open(new File("git/"));
+		assertEquals("class org.eclipse.jgit.internal.storage.file.FileRepository", g.getRepository().getClass().toString());
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValuesList() {
+		String t = "timestamp";
+		String f = "filename";
+		assertEquals("timestamp", t);
+		assertEquals("filename", f);
+		
+		
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopulateHTML() {
+		String html = "<html>";
+		String table = "<table>";
+		assertEquals("<table", table);
+		assertEquals("<html>", html);
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		App app = new App ();
+		assertEquals("ES2.CovidGraph.App", app.getClass().toString());
+		//fail("Not yet implemented");
+	}
+
 }
